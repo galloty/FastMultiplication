c3fd357fc1d87170e2d8cfaf87b380a859f7a563
galloty <galloty@orange.fr>
2025-07-03 10:02:05 +0200

    fastMul_rec235

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Fast modular multiplication of polynomials is a divide-and-conquer algorithm, wh
 
 The recursion is applied to compute *P*(*x*)<sup>2</sup> mod&nbsp;*x*<sup>8</sup>&nbsp;-&nbsp;7, where *P* is a polynomial of degree 8-1. The field is the complex numbers. The Chinese remainder theorem for polynomials is evaluated: at each step, *P*(*x*)<sup>2</sup> mod&nbsp;*x*<sup>2*m*</sup>&nbsp;-&nbsp;*r*<sup>2*m*</sup> is calculated from *P*(*x*)<sup>2</sup> mod&nbsp;*x*<sup>*m*</sup>&nbsp;-&nbsp;*r*<sup>*m*</sup> and *P*(*x*)<sup>2</sup> mod&nbsp;*x*<sup>*m*</sup>&nbsp;+&nbsp;*r*<sup>*m*</sup>. The last step is Lagrange interpolation and we have *P*(*x*) mod&nbsp;*x*&nbsp;-&nbsp;*r* = *P*(*r*).
 
-## *fastMul_rec235.cpp*
+## [*fastMul_rec235.cpp*](fastMul_rec235.cpp)
 
 The algorithm is extended to *P*(*x*)<sup>2</sup> mod&nbsp;*x*<sup>2<sup>2</sup>&nbsp;&middot;&nbsp;3<sup>2</sup>&nbsp;&middot;&nbsp;5<sup>2</sup></sup>&nbsp;-&nbsp;7. We have *x*<sup>3*m*</sup>&nbsp;-&nbsp;*r*<sup>3*m*</sup> = (x&nbsp;-&nbsp;*r*)&nbsp;&middot;&nbsp;(x&nbsp;-&nbsp;*jr*)&nbsp;&middot;&nbsp;(x&nbsp;-&nbsp;*j*<sup>2</sup>*r*)&nbsp;&middot;&nbsp;, where *j* is a primitive root of *x*<sup>3</sup>&nbsp;-&nbsp;1</sup>. The equivalent relation is applied to *x*<sup>5*m*</sup>&nbsp;-&nbsp;*r*<sup>5*m*</sup>.
 
--- fastMul_rec235.cpp
+++ fastMul_rec235.cpp
@@ -20,38 +20,30 @@ static void square_fast(Complex * const P, const size_t m, const Complex & r)
 {
 	if (m == 1) { P[0] *= P[0]; return; } 	// P is a scalar
 
-	if (m % 5 == 0)
+	if (m % 2 == 0)
 	{
-		// R = R0 * R1 * R2 * R3 * R4, where Ri = x^{m/3} - ri
-		const Complex K = std::polar(1.0, 2 * M_PI / 5), K2 = K * K, K3 = K * K2, K4 = K2 * K2, r0 = std::pow(r, 1.0 / 5);	// ri = K^i * r0
-		const Complex r02 = r0 * r0, r03 = r0 * r02, r04 = r02 * r02;
+		// R = R0 * R1, where Ri = x^{m/2} - ri
+		const Complex r0 = std::sqrt(r);	// r1 = -r0;
 
-		// Pi = P mod Ri
-		Complex * const P0 = &P[0 * m / 5]; Complex * const P1 = &P[1 * m / 5];
-		Complex * const P2 = &P[2 * m / 5]; Complex * const P3 = &P[3 * m / 5];  Complex * const P4 = &P[4 * m / 5];
+		// Pi = P mod Ri: if P = a[0] + a[1].x + ... then Pi mod (x^{m/2} - ri) = (a[0] + ri * a[m/2]) + ...
+		Complex * const P0 = &P[0 * m / 2]; Complex * const P1 = &P[1 * m / 2];
 
-		for (size_t i = 0; i < m / 5; ++i)
+		for (size_t i = 0; i < m / 2; ++i)
 		{
-			const Complex u0 = P0[i], u1 = r0 * P1[i], u2 = r02 * P2[i], u3 = r03 * P3[i], u4 = r04 * P4[i];
-			P0[i] = u0 +      u1 +      u2 +      u3 +      u4;
-			P1[i] = u0 + K  * u1 + K2 * u2 + K3 * u3 + K4 * u4;
-			P2[i] = u0 + K2 * u1 + K4 * u2 + K  * u3 + K3 * u4;
-			P3[i] = u0 + K3 * u1 + K  * u2 + K4 * u3 + K2 * u4;
-			P4[i] = u0 + K4 * u1 + K3 * u2 + K2 * u3 + K  * u4;
+			const Complex u0 = P0[i], u1 = r0 * P1[i];
+			P0[i] = u0 + u1;
+			P1[i] = u0 - u1;
 		}
 
-		// Pi = P^2 mod Ri => divide and conquer
-		square_fast(P0, m / 5, r0); square_fast(P1, m / 5, K * r0); square_fast(P2, m / 5, K2 * r0); square_fast(P3, m / 5, K3 * r0); square_fast(P4, m / 5, K4 * r0);
+		// P1 = P^2 mod R1, P2 = P^2 mod R2 => divide and conquer
+		square_fast(P0, m / 2, r0); square_fast(P1, m / 2, -r0);
 
 		// Chinese remainder theorem or simply the inverse of direct butterfly
-		for (size_t i = 0; i < m / 5; ++i)
+		for (size_t i = 0; i < m / 2; ++i)
 		{
-			const Complex u0 = P0[i], u1 = P1[i], u2 = P2[i], u3 = P3[i], u4 = P4[i];
-			P0[i] = (u0 +      u1 +      u2 +      u3 +      u4) /  5.0;
-			P1[i] = (u0 + K4 * u1 + K3 * u2 + K2 * u3 + K  * u4) / (5.0 * r0);
-			P2[i] = (u0 + K3 * u1 + K  * u2 + K4 * u3 + K2 * u4) / (5.0 * r02);
-			P3[i] = (u0 + K2 * u1 + K4 * u2 + K  * u3 + K3 * u4) / (5.0 * r03);
-			P4[i] = (u0 + K  * u1 + K2 * u2 + K3 * u3 + K4 * u4) / (5.0 * r04);
+			const Complex u0 = P0[i], u1 = P1[i];
+			P0[i] = (u1 + u0) /  2.0;
+			P1[i] = (u0 - u1) / (2.0 * r0);
 		}
 	}
 	else if (m % 3 == 0)
@@ -83,30 +75,38 @@ static void square_fast(Complex * const P, const size_t m, const Complex & r)
 			P2[i] = (u0 + J  * u1 + J2 * u2) / (3.0 * r02);
 		}
 	}
-	else if (m % 2 == 0)
+	else if (m % 5 == 0)
 	{
-		// R = R0 * R1, where Ri = x^{m/2} - ri
-		const Complex r0 = std::sqrt(r);	// r1 = -r0;
+		// R = R0 * R1 * R2 * R3 * R4, where Ri = x^{m/3} - ri
+		const Complex K = std::polar(1.0, 2 * M_PI / 5), K2 = K * K, K3 = K * K2, K4 = K2 * K2, r0 = std::pow(r, 1.0 / 5);	// ri = K^i * r0
+		const Complex r02 = r0 * r0, r03 = r0 * r02, r04 = r02 * r02;
 
-		// Pi = P mod Ri: if P = a[0] + a[1].x + ... then Pi mod (x^{m/2} - ri) = (a[0] + ri * a[m/2]) + ...
-		Complex * const P0 = &P[0 * m / 2]; Complex * const P1 = &P[1 * m / 2];
+		// Pi = P mod Ri
+		Complex * const P0 = &P[0 * m / 5]; Complex * const P1 = &P[1 * m / 5];
+		Complex * const P2 = &P[2 * m / 5]; Complex * const P3 = &P[3 * m / 5];  Complex * const P4 = &P[4 * m / 5];
 
-		for (size_t i = 0; i < m / 2; ++i)
+		for (size_t i = 0; i < m / 5; ++i)
 		{
-			const Complex u0 = P0[i], u1 = r0 * P1[i];
-			P0[i] = u0 + u1;
-			P1[i] = u0 - u1;
+			const Complex u0 = P0[i], u1 = r0 * P1[i], u2 = r02 * P2[i], u3 = r03 * P3[i], u4 = r04 * P4[i];
+			P0[i] = u0 +      u1 +      u2 +      u3 +      u4;
+			P1[i] = u0 + K  * u1 + K2 * u2 + K3 * u3 + K4 * u4;
+			P2[i] = u0 + K2 * u1 + K4 * u2 + K  * u3 + K3 * u4;
+			P3[i] = u0 + K3 * u1 + K  * u2 + K4 * u3 + K2 * u4;
+			P4[i] = u0 + K4 * u1 + K3 * u2 + K2 * u3 + K  * u4;
 		}
 
-		// P1 = P^2 mod R1, P2 = P^2 mod R2 => divide and conquer
-		square_fast(P0, m / 2, r0); square_fast(P1, m / 2, -r0);
+		// Pi = P^2 mod Ri => divide and conquer
+		square_fast(P0, m / 5, r0); square_fast(P1, m / 5, K * r0); square_fast(P2, m / 5, K2 * r0); square_fast(P3, m / 5, K3 * r0); square_fast(P4, m / 5, K4 * r0);
 
 		// Chinese remainder theorem or simply the inverse of direct butterfly
-		for (size_t i = 0; i < m / 2; ++i)
+		for (size_t i = 0; i < m / 5; ++i)
 		{
-			const Complex u0 = P0[i], u1 = P1[i];
-			P0[i] = (u1 + u0) /  2.0;
-			P1[i] = (u0 - u1) / (2.0 * r0);
+			const Complex u0 = P0[i], u1 = P1[i], u2 = P2[i], u3 = P3[i], u4 = P4[i];
+			P0[i] = (u0 +      u1 +      u2 +      u3 +      u4) /  5.0;
+			P1[i] = (u0 + K4 * u1 + K3 * u2 + K2 * u3 + K  * u4) / (5.0 * r0);
+			P2[i] = (u0 + K3 * u1 + K  * u2 + K4 * u3 + K2 * u4) / (5.0 * r02);
+			P3[i] = (u0 + K2 * u1 + K4 * u2 + K  * u3 + K3 * u4) / (5.0 * r03);
+			P4[i] = (u0 + K  * u1 + K2 * u2 + K3 * u3 + K4 * u4) / (5.0 * r04);
 		}
 	}
 }
