1d485e19b48224ec9ec62f55ffccac4f98057c15
galloty <galloty@orange.fr>
2025-07-07 14:24:50 +0200

    fastMul_GF

--- fastMul_GF.cpp
+++ fastMul_GF.cpp
@@ -7,178 +7,312 @@ Please give feedback to the authors if improvement is realized. It is distribute
 
 #include <cstdint>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-// Modulo 2^61 - 1
-class Zp
-{
-private:
-	static const uint64_t _p = (uint64_t(1) << 61) - 1;
-	uint64_t _n;
-
-	static uint64_t add(const uint64_t a, const uint64_t b) { return a + b - ((a >= _p - b) ? _p : 0); }
-	static uint64_t sub(const uint64_t a, const uint64_t b) { return a - b + ((a < b) ? _p : 0); }
-	static uint64_t mul(const uint64_t a, const uint64_t b) { return uint64_t((a * __uint128_t(b)) % _p); }
-
-public:
-	Zp() {}
-	Zp(const uint64_t n) : _n(n) {}
-
-	uint64_t get() const { return _n; }
-	int64_t get_int() const { return (_n >= _p / 2) ? int64_t(_n - _p) : int64_t(_n); }
-
-	Zp operator-() const { return Zp((_n == 0) ? 0 : _p - _n); }
-
-	Zp operator+(const Zp & rhs) const { return Zp(add(_n, rhs._n)); }
-	Zp operator-(const Zp & rhs) const { return Zp(sub(_n, rhs._n)); }
-	Zp operator*(const Zp & rhs) const { return Zp(mul(_n, rhs._n)); }
-
-	Zp & operator*=(const Zp & rhs) { _n = mul(_n, rhs._n); return *this; }
-
-	Zp pow(const uint64_t e) const
-	{
-		if (e == 0) return Zp(1);
-		Zp r = Zp(1), y = *this;
-		for (uint64_t i = e; i != 1; i /= 2) { if (i % 2 != 0) r *= y; y *= y; }
-		return r * y;
-	}
-
-	Zp invert() const { return pow(_p - 2); }
-};
-
-// GF((2^61 - 1)^2)
+// a prime finite field such that p = k * n + 1 and a nth root of r exists
 class GF
 {
 private:
-	Zp _a, _b;
-	static const uint64_t _h_a = 2147483648ull, _h_b = 1272521237944691271ull;	// a primitive root of order 2^62 which is a root of (0, 1).
+	uint32_t n;
 
-	GF(const Zp & a, const Zp & b) : _a(a), _b(b) {}
+public:
+	// Let p be the order of the finite field. We must have:
+	// - p > n * (b - 1)^2, where n is the transform size and b the base representation.
+	// - a nth root of r must exist.
+	// If b = 1000, n = 2^2 * 3^2 * 5^2 and r = 7, the first condition is p > 898200900.
+	// If p = 913262401 then 243771734^m = 7 (mod p).
+
+	static const uint32_t _p = 913262401u;
+	static const uint32_t _primroot = 11u;
+	static const uint32_t _root1 = 422058578u;	// 422058578^n = 1
+	static const uint32_t _root7 = 243771734u;	// 243771734^n = 7
 
 public:
 	GF() {}
-	GF(const int64_t a, const int64_t b) : _a(uint64_t(a)), _b(uint64_t(b)) {}	// a, b >= 0
+	GF(const uint32_t l) : n(l) {}
 
-	const Zp & a() const { return _a; }
-	const Zp & b() const { return _b; }
+	uint32_t get() const { return n; }
 
-	GF conj() const { return GF(_a, -_b); }
+	GF & operator+=(const GF & rhs) { const uint32_t c = (n >= _p - rhs.n) ? _p : 0; n += rhs.n; n -= c; return *this; }
+	GF & operator-=(const GF & rhs) { const uint32_t c = (n < rhs.n) ? _p : 0; n -= rhs.n; n += c; return *this; }
+	GF & operator*=(const GF & rhs) { *this = uint32_t((n * uint64_t(rhs.n)) % _p); return *this; }
 
-	GF operator+(const GF & rhs) const { return GF(_a + rhs._a, _b + rhs._b); }
-	GF operator-(const GF & rhs) const { return GF(_a - rhs._a, _b - rhs._b); }
-	GF operator*(const GF & rhs) const { return GF(_a * rhs._a - _b * rhs._b, _a * rhs._b + _b * rhs._a); }
+	GF operator+(const GF & rhs) const { GF r = *this; r += rhs; return r; }
+	GF operator-(const GF & rhs) const { GF r = *this; r -= rhs; return r; }
+	GF operator*(const GF & rhs) const { GF r = *this; r *= rhs; return r; }
 
-	GF & operator*=(const GF & rhs) { *this = *this * rhs; return *this; }
-
-	// GF invert() const
-	// {
-	// 	const Zp t = Zp(_a * _a + _b * _b).invert();
-	// 	return GF(_a * t, (-_b) * t);
-	// }
-
-	GF pow(const uint64_t e) const
+	GF pow(const uint32_t e) const
 	{
-		if (e == 0) return GF(1, 0);
-		GF r = GF(1, 0), y = *this;
-		for (uint64_t i = e; i != 1; i /= 2) { if (i % 2 != 0) r *= y; y *= y; }
-		return r * y;
+		if (e == 0) return GF(1u);
+
+		GF r = GF(1u), y = *this;
+		for (uint32_t i = e; i != 1; i /= 2)
+		{
+			if (i % 2 != 0) r *= y;
+			y *= y;
+		}
+		r *= y;
+
+		return r;
 	}
 
-	static const GF primroot_n(const uint64_t n) { return GF(Zp(_h_a), Zp(_h_b)).pow((uint64_t(1) << 62) / n); }
+	GF invert() const { return pow(_p - 2); }
+
+	static const GF root_900th_1() { return GF(_root1); }
+	static const GF root_900th_7() { return GF(_root7); }
+	static GF root_nth(const uint32_t n) { return GF(_primroot).pow((_p - 1) / n); }
 };
 
-constexpr size_t bitrev(const size_t i, const size_t n)
+// P = P^2 mod x^n - r
+class conv_fast
 {
-	size_t r = 0; for (size_t k = n, j = i; k > 1; k /= 2, j /= 2) r = (2 * r) | (j % 2); return r;
-}
+private:
+	const size_t _n;
+	GF * const _root;
+	GF * const _invroot;
+	const GF J = GF::root_nth(3u), J2 = J * J;
+	const GF K = GF::root_nth(5u), K2 = K * K, K3 = K * K2, K4 = K2 * K2;
 
-static void roots(GF * const w, const size_t n)
-{
-	// Note that:
-	// - w[1] = 2^30 * (1, 1)
-	// - if w[2k] = (a, b) then w[2k + 1] = (-b, a)
-	for (size_t s = 1; s < n; s *= 2)
+	static constexpr size_t reversal(const size_t i, const size_t n)
 	{
-		const GF r_s = GF::primroot_n(8 * s);
+		size_t r = 0, k = n, j = i;
+		for (; k % 2 == 0; k /= 2, j /= 2) r = 2 * r + j % 2;
+		for (; k % 3 == 0; k /= 3, j /= 3) r = 3 * r + j % 3;
+		for (; k % 5 == 0; k /= 5, j /= 5) r = 5 * r + j % 5;
+		return r;
+	}
+
+	void set_roots(const size_t s, const size_t m) const
+	{
+		GF * const root = _root;
+		GF * const invroot = _invroot;
 		for (size_t j = 0; j < s; ++j)
 		{
-			w[s + j] = r_s.pow(bitrev(j, 4 * s) + 1);
-			// std::cout << s << ", " << j << ": " << w[s + j].a().get_int() << ", " << w[s + j].b().get_int() << std::endl;
+			const GF r = (GF::root_900th_7() * GF::root_900th_1().pow(reversal(j, s))).pow(m);
+			root[s + j] = r; invroot[s + j] = r.invert();
 		}
 	}
-}
 
-static void forward2(GF * const x, const GF * const w, const size_t n)
-{
-	for (size_t s = 1, m = n / 2; m >= 1; s *= 2, m /= 2)
+public:
+	conv_fast(const size_t n) : _n(n), _root(new GF[n]), _invroot(new GF[n])
 	{
-		for (size_t j = 0; j < s; ++j)
+		size_t m = _n, s = 1;
+		while (m != 1)
 		{
-			const GF w_sj = w[s + j];
+			if      (m % 5 == 0) { m /= 5; set_roots(s, m); s *= 5; }
+			else if (m % 3 == 0) { m /= 3; set_roots(s, m); s *= 3; }
+			else if (m % 2 == 0) { m /= 2; set_roots(s, m); s *= 2; }
+		}
+	}
 
-			for (size_t i = 0; i < m; ++i)
+	void square(GF * const P) const
+	{
+		const size_t n = _n;
+		const GF * const root = _root;
+
+		size_t m = n, s = 1;
+		while (m != 1)
+		{
+			if (m % 5 == 0)
 			{
-				const size_t k = j * 2 * m + i;
-				const GF u0 = x[k + 0 * m], u1 = x[k + 1 * m] * w_sj;
-				x[k + 0 * m] = u0 + u1; x[k + 1 * m] = u0 - u1;
+				m /= 5;
+
+				for (size_t j = 0; j < s; ++j)
+				{
+					const GF r = root[s + j], r2 = r * r, r03 = r * r2, r4 = r2 * r2;
+
+					for (size_t i = 0; i < m; ++i)
+					{
+						const size_t k = 5 * m * j + i;
+						const GF u0 = P[k + 0 * m], u1 = r * P[k + 1 * m], u2 = r2 * P[k + 2 * m], u3 = r03 * P[k + 3 * m], u4 = r4 * P[k + 4 * m];
+						P[k + 0 * m] = u0 +      u1 +      u2 +      u3 +      u4;
+						P[k + 1 * m] = u0 + K  * u1 + K2 * u2 + K3 * u3 + K4 * u4;
+						P[k + 2 * m] = u0 + K2 * u1 + K4 * u2 + K  * u3 + K3 * u4;
+						P[k + 3 * m] = u0 + K3 * u1 + K  * u2 + K4 * u3 + K2 * u4;
+						P[k + 4 * m] = u0 + K4 * u1 + K3 * u2 + K2 * u3 + K  * u4;
+					}
+				}
+
+				s *= 5;
+			}
+			else if (m % 3 == 0)
+			{
+				m /= 3;
+
+				for (size_t j = 0; j < s; ++j)
+				{
+					const GF r = root[s + j], r2 = r * r;
+
+					for (size_t i = 0; i < m; ++i)
+					{
+						const size_t k = 3 * m * j + i;
+						const GF u0 = P[k + 0 * m], u1 = r * P[k + 1 * m], u2 = r2 * P[k + 2 * m];
+						P[k + 0 * m] = u0 +      u1 +      u2;
+						P[k + 1 * m] = u0 + J  * u1 + J2 * u2;
+						P[k + 2 * m] = u0 + J2 * u1 + J  * u2;
+					}
+				}
+
+				s *= 3;
+			}
+			else if (m % 2 == 0)
+			{
+				m /= 2;
+
+				for (size_t j = 0; j < s; ++j)
+				{
+					const GF r = root[s + j];
+
+					for (size_t i = 0; i < m; ++i)
+					{
+						const size_t k = 2 * m * j + i;
+						const GF u0 = P[k + 0 * m], u1 = r * P[k + 1 * m];
+						P[k + 0 * m] = u0 + u1;
+						P[k + 1 * m] = u0 - u1;
+					}
+				}
+
+				s *= 2;
 			}
 		}
-	}
-}
 
-static void backward2(GF * const x, const GF * const w, const size_t n)
-{
-	for (size_t s = n / 2, m = 1; s >= 1; s /= 2, m *= 2)
-	{
-		for (size_t j = 0; j < s; ++j)
+		for (size_t k = 0; k < n; ++k) P[k] *= P[k];
+
+		const GF * const invroot = _invroot;
+		// size_t m = 1, s = _n;
+		while (s != 1)
 		{
-			const GF w_sj_inv = w[s + j].conj();	// conjugate is inverse
-
-			for (size_t i = 0; i < m; ++i)
+			if (s % 2 == 0)
 			{
-				const size_t k = j * 2 * m + i;
-				const GF u0 = x[k + 0 * m], u1 = x[k + 1 * m];
-				x[k + 0 * m] = u0 + u1; x[k + 1 * m] = (u0 - u1) * w_sj_inv;
+				s /= 2;
+
+				for (size_t j = 0; j < s; ++j)
+				{
+					const GF invr = invroot[s + j];
+
+					for (size_t i = 0; i < m; ++i)
+					{
+						const size_t k = 2 * m * j + i;
+						const GF u0 = P[k + 0 * m], u1 = P[k + 1 * m];
+						P[k + 0 * m] =  u1 + u0;
+						P[k + 1 * m] = (u0 - u1) * invr;
+					}
+				}
+
+				m *= 2;
+			}
+			else if (s % 3 == 0)
+			{
+				s /= 3;
+
+				for (size_t j = 0; j < s; ++j)
+				{
+					const GF invr = invroot[s + j], invr2 = invr * invr;
+
+					for (size_t i = 0; i < m; ++i)
+					{
+						const size_t k = 3 * m * j + i;
+						const GF u0 = P[k + 0 * m], u1 = P[k + 1 * m], u2 = P[k + 2 * m];
+						P[k + 0 * m] =  u0 +      u1 +      u2;
+						P[k + 1 * m] = (u0 + J2 * u1 + J  * u2) * invr;
+						P[k + 2 * m] = (u0 + J  * u1 + J2 * u2) * invr2;
+					}
+				}
+
+				m *= 3;
+			}
+			else if (s % 5 == 0)
+			{
+				s /= 5;
+
+				for (size_t j = 0; j < s; ++j)
+				{
+					const GF invr = invroot[s + j], invr2 = invr * invr, invr3 = invr * invr2, invr4 = invr2 * invr2;
+
+					for (size_t i = 0; i < m; ++i)
+					{
+						const size_t k = 5 * m * j + i;
+						const GF u0 = P[k + 0 * m], u1 = P[k + 1 * m], u2 = P[k + 2 * m], u3 = P[k + 3 * m], u4 = P[k + 4 * m];
+						P[k + 0 * m] =  u0 +      u1 +      u2 +      u3 +      u4;
+						P[k + 1 * m] = (u0 + K4 * u1 + K3 * u2 + K2 * u3 + K  * u4) * invr;
+						P[k + 2 * m] = (u0 + K3 * u1 + K  * u2 + K4 * u3 + K2 * u4) * invr2;
+						P[k + 3 * m] = (u0 + K2 * u1 + K4 * u2 + K  * u3 + K3 * u4) * invr3;
+						P[k + 4 * m] = (u0 + K  * u1 + K2 * u2 + K3 * u3 + K4 * u4) * invr4;
+					}
+				}
+
+				m *= 5;
 			}
 		}
+
+		const GF invn = GF(uint32_t(n)).invert();
+		for (size_t k = 0; k < n; ++k) P[k] *= invn;
+	}
+};
+
+// Q = P^2 mod x^n - r
+static void square_slow(GF * const Q, const GF * const P, const size_t n, const GF & r)
+{
+	for (size_t j = 0; j < n; ++j)
+	{
+		GF l = 0; for (size_t i = 0; i <= j; ++i) l += P[i] * P[j - i];
+		GF h = 0; for (size_t i = j + 1; i < n; ++i) h += P[i] * P[j - i + n];
+		Q[j] = l + r * h;
 	}
 }
 
-static void square(GF * const x, const size_t n)
+inline void check(const GF * const P, const GF * const Q, const size_t n)
 {
-	for (size_t k = 0; k < n; ++k) x[k] *= x[k];
+	bool error = false;
+	uint32_t a_max = 0;
+	for (size_t i = 0; i < n; ++i)
+	{
+		const uint32_t a = P[i].get(), b = Q[i].get();
+		if (a != b) { error = true; std::cout << i << ": " << a << " != " << b << std::endl; }
+		a_max = std::max(a_max, a);
+	}
+	std::cout << (error ? "N" : "") << "OK, max = " << a_max << std::endl;
+}
+
+inline void display(const GF * const P, const size_t n)
+{
+	for (size_t i = n - 1, j = 0; j < n; --i, ++j)
+	{
+		std::cout << P[i].get();
+		if (i > 0)
+		{
+			std::cout << "*x";
+			if (i > 1) std::cout << "^" << i;
+			std::cout << " + ";
+		}
+	} 
 }
 
 int main()
 {
-	const size_t n = 16;
+	std::srand((unsigned int)(std::time(nullptr)));
 
-	// (25M * (n - 1) + 1)^2 * n = 2.25e18 < 2^61 - 1 = 2.30e18
-	int64_t a[n]; for (size_t i = 0; i < n; ++i) a[i] = 25000000*i + 1;
+	const size_t n = 2*2*3*3*5*5;
+	GF P[n]; for (size_t i = 0; i < n; ++i) P[i] = GF(uint32_t(std::rand()) % 1000u);
+	const GF r = GF(7u);	// modulo x^n - r
 
-	GF w[n / 2]; roots(w, n / 2);
+	// std::cout << "Mod(";
+	// display(P, n);
+	// std::cout << ", x^" << n << " - " << r.get() << ")^2" << std::endl;
 
-	// A transform of length n / 2 can be used because the polynomial of GF is X^2 + 1
-	GF x[n / 2]; for (size_t i = 0; i < n / 2; ++i) x[i] = GF(a[i], a[i + n / 2]);
+	GF Q[n]; square_slow(Q, P, n, r);
+	// std::cout << " = ";
+	// display(Q, n);
+	// std::cout << std::endl;
 
-	// Can be optimized using Radix-4 which has 3/4 as many GF multiplies as Radix-2
-	forward2(x, w, n / 2);
-	square(x, n / 2);
-	backward2(x, w, n / 2);
+	conv_fast conv(n);
+	conv.square(P);
+	// std::cout << " = ";
+	// display(P, n);
+	// std::cout << std::endl;
 
-	std::cout << "Mod(1 + 25000001*x + 50000001*x^2 + 75000001*x^3 + 100000001*x^4 + 125000001*x^5 + 150000001*x^6 + 175000001*x^7 + 200000001*x^8 + 225000001*x^9 + 250000001*x^10 + 275000001*x^11 + 300000001*x^12 + 325000001*x^13 + 350000001*x^14 + 375000001*x^15, x^16 + 1)^2" << std::endl;
-	std::cout << " = 350000006000000016*x^15 + 143750004500000014*x^14 - 34999996899999988*x^13 - 187499998199999990*x^12 - 314999999399999992*x^11 - 418750000499999994*x^10 - 500000001499999996*x^9 - 560000002399999998*x^8 - 600000003200000000*x^7 - 621250003900000002*x^6 - 625000004500000004*x^5 - 612500005000000006*x^4 - 585000005400000008*x^3 - 543750005700000010*x^2 - 490000005900000012*x - 425000006000000014" << std::endl << "   ";
-
-	const Zp r = Zp(n / 2).invert();
-	for (size_t i = n - 1, j = 0; j < n; --i, ++j)
-	{
-		const auto a_j = (i >= n / 2) ? x[i - n / 2].b() : x[i].a();
-		const int64_t k = Zp(a_j * r).get_int();
-
-		if (j != 0) std::cout << ((k >= 0) ? " + ": " - ");
-		std::cout << std::abs(k);
-		if (i > 1) std::cout << "*x^" << i; else if (i > 0) std::cout << "*x";
-	} 
-	std::cout << std::endl;
+	check(P, Q, n);
 
 	return EXIT_SUCCESS;
 }
